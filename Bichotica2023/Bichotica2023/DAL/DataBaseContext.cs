93c9b3e5a1b1cdd6f3ae5aa3ddfb66498a5ca168
sebastian bedoya gil <sbedoygi@gmail.com>
2023-11-11 11:34:28 -0500

    Creation of Index Tables for Entity Framework

---
+++ Bichotica2023/Bichotica2023/DAL/DataBaseContext.cs
@@ -0,0 +1,24 @@
+﻿using Bichotica2023.DAL.Entities;
+using System.Collections.Generic;
+using System.Reflection.Emit;
+
+namespace Bichotica2023.DAL
+{
+    public class DataBaseContext : DbContext
+    {
+        public DataBaseContext(DbContextOptions<DataBaseContext> options) : base(options)
+        {
+        }
+
+        // se crea el constexto para las tablas de la base de datos
+        public DbSet<Ticket> Tickets { get; set; }
+
+       
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Ticket>().HasIndex(t => t.TicketID).IsUnique();
+        }
+
+    }
+}
