70df4e2528c8416c04116c7822c39061687d5fcb
sebastian bedoya gil <sbedoygi@gmail.com>
2023-11-11 11:27:43 -0500

    Creation of identity: Enter your Ticket for the database

---
+++ Bichotica2023/Bichotica2023/DAL/Entities/Ticket.cs
@@ -0,0 +1,36 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Bichotica2023.DAL.Entities
+{
+    public class Ticket : Entity
+    {
+        #region
+        [Display(Name = "Ingrese su Ticket")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio")]
+        public int TicketID { get; set; }
+
+        [Display(Name = "Confirmacion Fecha Activación")]
+        public DateTime? UseDate { get; set; }
+
+        [Display(Name = "Ticket Activo")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio")]
+        public bool IsUsed { get; set; }
+
+        [Display(Name = "Selecione nombre de la entrada es requerido")]
+    
+        public Entrance? EntranceGate { get; set; }
+
+        public enum Entrance
+        {
+            Oriental,
+            Occidental,
+            Norte,
+            Sur,
+
+        }
+      
+
+
+    }
+    #endregion
+}
