d75bf7f9c3e4067609f173354a69cc2c87716562
sebastian bedoya gil <sbedoygi@gmail.com>
2023-11-11 11:10:44 -0500

    Creation of folders for the entities in DAL.Entities.

---
+++ Bichotica2023/Bichotica2023/DAL/Entities/Entity.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Bichotica2023.DAL.Entities
+{
+    public class Entity
+    {
+        [Key]
+        public virtual Guid Id { get; set; }
+
+        [Display(Name = "Fecha Creación")]
+        public virtual DateTime? CreateDate { get; set; }
+
+        [Display(Name = "Fecha Modificación")]
+        public virtual DateTime? ModifiedDate { get; set; }
+    }
+}
+
+
