3d80fef799c60153d6a35c733ebac5f8c7d54198
sebastian bedoya gil <sbedoygi@gmail.com>
2023-11-11 11:50:32 -0500

    NuGet commands for the initial creation of tables

---
+++ Bichotica2023/Bichotica2023/Migrations/20231111165022_initialCreation.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Bichotica2023.Migrations
+{
+    /// <inheritdoc />
+    public partial class initialCreation : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Tickets",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    TicketID = table.Column<int>(type: "int", nullable: false),
+                    UseDate = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    IsUsed = table.Column<bool>(type: "bit", nullable: false),
+                    EntranceGate = table.Column<int>(type: "int", nullable: true),
+                    CreateDate = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    ModifiedDate = table.Column<DateTime>(type: "datetime2", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Tickets", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Tickets_TicketID",
+                table: "Tickets",
+                column: "TicketID",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Tickets");
+        }
+    }
+}
